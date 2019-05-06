bea5cf3762cec0aba78ce1f7d55d501030c3a23e
Robert H <robertjameshardin@aol.com>
2019-05-05 20:34:54 -0500

    Still working on sequelize not defined, fixed a pathing issues with a node_mudles dependency

--- models/index.js
+++ models/index.js
@@ -1,11 +1,11 @@
 "use strict"; 
+require("dotenv").config();
 const fs = require("fs");
 const path = require("path");
 const Sequelize = require("sequelize");
 const basename = path.basename(module.filename);
 const env = process.env.NODE_ENV || "development";
 const config = require(__dirname + "/../config/config.json")[env];
-require("dotenv").config();
 const db = {};
 if (config.use_env_variable) {
   let sequelize = new Sequelize(process.env[config.use_env_variable]);
