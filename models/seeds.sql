e2bf14f36c9121c00af047a869ed4940e6e0482e
Robert H <robertjameshardin@aol.com>
2019-05-08 18:56:00 -0500

    still troubleshooting travis, removed failed portion of build

--- models/seeds.sql
+++ models/seeds.sql
@@ -1,11 +1,12 @@
-INSERT INTO movie (title, posterURL)
+use movies_db;
+INSERT INTO Movies (title, posterURL)
     VALUES  ("Avengers: Infinity War", "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SY1000_CR0,0,674,1000_AL_.jpg"),
             ("Clue", "https://m.media-amazon.com/images/M/MV5BM2VlNTE1ZmMtOTAyNS00ODYwLWFmY2MtZWEzOTE2YjE1NDE2XkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_SY1000_CR0,0,658,1000_AL_.jpg"),
             ("Jurassic Park", "https://m.media-amazon.com/images/M/MV5BMjM2MDgxMDg0Nl5BMl5BanBnXkFtZTgwNTM2OTM5NDE@._V1_.jpg"),
             ("Apocalypse Now", "https://m.media-amazon.com/images/M/MV5BZTNkZmU0ZWQtZjQzMy00YTNmLWFmN2MtZGNkMmU1OThmMGYwXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SY1000_CR0,0,652,1000_AL_.jpg"),
             ("The Sixth Sense", "https://m.media-amazon.com/images/M/MV5BMWM4NTFhYjctNzUyNi00NGMwLTk3NTYtMDIyNTZmMzRlYmQyXkEyXkFqcGdeQXVyMTAwMzUyOTc@._V1_.jpg");
 
-INSERT INTO review (text, spoiler, MovieId)
+INSERT INTO Reviews (text, spoiler, MovieId)
     VALUES  ("The Butler did it", false, 2),
             ("Bruce Willis is already dead", true, 5),
             ("The bad guy wins", true, 4),
