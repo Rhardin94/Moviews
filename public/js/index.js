8e9f9cbe1688710dad366804f62bbe2bf29111a7
DenSZou <dzou@u.northwestern.edu>
2019-05-08 15:11:54 -0500

    handlebars, waiting on javascript display logic

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,3 @@
-// Get references to page elements
 //var $exampleText = $("#example-text");
 //var $exampleDescription = $("#example-description");
 //var $submitBtn = $("#submit");
@@ -97,4 +96,3 @@ var handleDeleteBtnClick = function() {
 // Add event listeners to the submit and delete buttons
 $submitBtn.on("click", handleFormSubmit);
 $exampleList.on("click", ".delete", handleDeleteBtnClick);
-
